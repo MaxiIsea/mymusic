b9fb12c434ab13fbf161c29af85ced86700d8237
MaxiIsea <max.nirvana22@hotmail.com>
2021-04-27 19:03:56 -0300

    JerseyConfig Added

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusic/JerseyConfig.java
@@ -0,0 +1,8 @@
+package ar.edu.unnoba.pdyc.mymusic;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+}
